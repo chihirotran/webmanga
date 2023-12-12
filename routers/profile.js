9ee6e8c30807a9204ac55a50b082a848082dad05
NguyenNgocVuong123 <112496039+NguyenNgocVuong123@users.noreply.github.com>
2023-12-12 11:44:27 +0700

    profile and add chapter

--- routers/profile.js
+++ routers/profile.js
@@ -16,5 +16,64 @@ router.get("/profile",async(req,res)=>{
     
     res.render('profile.ejs',{blogs,isLoggedIn,user,month,user_data,categoryy});
 })
-
+router.get("/profiledetail",async(req,res)=>{
+    const user=req.session.username;
+    const isLoggedIn=req.session.isLoggedIn;
+    const blogs=await Comic.find({author:user});
+    const user_data=await User.findOne({username:user});
+    const categoryy = req.session.catess;
+    // console.log(user_data);
+    
+    res.render('profiledetail.ejs',{blogs,isLoggedIn,user,month,user_data,categoryy});
+})
+router.get("/profilefollow",async(req,res)=>{
+    const user=req.session.username;
+    const isLoggedIn=req.session.isLoggedIn;
+    const blogs=await Comic.find({author:user});
+    const user_data=await User.findOne({username:user});
+    const categoryy = req.session.catess;
+    // console.log(user_data);
+    
+    res.render('profilefollow.ejs',{blogs,isLoggedIn,user,month,user_data,categoryy});
+})
+router.get("/profileupload",async(req,res)=>{
+    const user=req.session.username;
+    const isLoggedIn=req.session.isLoggedIn;
+    const blogs=await Comic.find({author:user});
+    const user_data=await User.findOne({username:user});
+    const categoryy = req.session.catess;
+    // console.log(user_data);
+    
+    res.render('profileupload.ejs',{blogs,isLoggedIn,user,month,user_data,categoryy});
+})
+router.get("/profilecomment",async(req,res)=>{
+    const user=req.session.username;
+    const isLoggedIn=req.session.isLoggedIn;
+    const blogs=await Comic.find({author:user});
+    const user_data=await User.findOne({username:user});
+    const categoryy = req.session.catess;
+    // console.log(user_data);
+    
+    res.render('profilecomment.ejs',{blogs,isLoggedIn,user,month,user_data,categoryy});
+})
+router.get("/profilenotice",async(req,res)=>{
+    const user=req.session.username;
+    const isLoggedIn=req.session.isLoggedIn;
+    const blogs=await Comic.find({author:user});
+    const user_data=await User.findOne({username:user});
+    const categoryy = req.session.catess;
+    // console.log(user_data);
+    
+    res.render('profilenotice.ejs',{blogs,isLoggedIn,user,month,user_data,categoryy});
+})
+router.get("/profilechangepass",async(req,res)=>{
+    const user=req.session.username;
+    const isLoggedIn=req.session.isLoggedIn;
+    const blogs=await Comic.find({author:user});
+    const user_data=await User.findOne({username:user});
+    const categoryy = req.session.catess;
+    // console.log(user_data);
+    
+    res.render('profilechangepass.ejs',{blogs,isLoggedIn,user,month,user_data,categoryy});
+})
 module.exports=router;
